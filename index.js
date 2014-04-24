2f53b26d33e0066fea36e4a142b23ccbe8e10f39
Chris Whitney <cwhitney@tarot.com>
2014-04-24 12:43:35 -0700

    just needs to download images

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var http = require('http');
 var _ = require('underscore');
 var parseString = require('xml2js').parseString;
 var prettyjson = require('prettyjson');
+var watcher;
 // pretty json example
 //console.log(prettyjson.render(json[x]));
 var storeID = process.argv[2];
@@ -66,11 +67,18 @@ function betterJson(obj, key, value) {
 
 function getFile(url, dest, callback) {
     var file = fs.createWriteStream(dest);
-    var request = http.get(url, function(response) {
+
+    var request = http.get(url, function(err, response) {
+        if(err){
+            console.log(err);
+        }
+        console.log(chunk);
         response.pipe(file);
         file.on('finish', function() {
             console.log('File downloaded to '+ dest);
-            file.close(callback);
+            if(typeof callback !== 'undefined'){
+                file.close(callback);
+            }
         });
     });
 }
@@ -83,7 +91,6 @@ function startXmlParse() {
                 console.log(err);
             }
             var catalog = parsedXml.Catalog;
-            console.log(catalog);
             var storeInfo = catalog.$;
             var table = catalog.Table;
             var items = catalog.Item;
@@ -108,24 +115,108 @@ function grabImageFields() {
                 }
             });
         }
-        console.log(imageFields.join(' '));
+        imageJson(imageFields);
     });
 }
 
-buildActions = {
+function imageJson(filedNames) {
+    var items = storeID + '/items.json';
+    fs.readFile(items, 'utf8', function (err, data){
+        var json = JSON.parse(data);
+        var newJson = {};
+        newJson.itemimages = [];
+        newJson.images = []
+        for (var x = 0; x < json.length; x++){
+            var itemID = json[x].$.ID;
+            var imageInfo = {};
+            var justImages = [];
+            var hasImage = false;
+            imageInfo.images = {};
+            imageInfo.id = itemID;
+            _.each(json[x].ItemField, function (item){
+                if (item.$.TableFieldID === 'name') {
+                    imageInfo.name = item.$.Value;
+                }
+                if (filedNames.indexOf(item.$.TableFieldID) >= 0 && item.$.Value.length > 0) {
+                    var clean_image = cleanImage(item.$.Value);
+                    imageInfo.images[item.$.TableFieldID] = clean_image;
+                    newJson.images.push(clean_image);
+                    hasImage = true;
+                }
+            });
+            if (hasImage) {
+                newJson.itemimages.push(imageInfo);
+            }
+        }
+        createFile('images.json', JSON.stringify(newJson));
+    });
+}
+
+function downloadImages() {
+    console.log('images dl!');
+    var images = storeID + '/images.json';
+    var file = fs.createWriteStream(storeID+'/images/');
+    //watcher.close(); // close as I dont need to watch images be downloaded.
+    fs.readFile(images, 'utf8', function (err, data){
+        if(err){
+            console.log(err);
+        }
+        var json = JSON.parse(data).images;
+        for(var x = 0; x < json.length; x++){
+            http.get(json[x], function(err, response) {
+                if(err){
+                    console.log(err);
+                }
+                response.pipe(file);
+                file.on('finish', function() {
+                    console.log('File downloaded');
+                });
+            });
+        }
+    });
+}
+
+function cleanImage(dirty) {
+    var clean = dirty.match(/http:\/\/[a-z\.\/0-9-]+/i);
+    return clean[0];
+}
+
+function startWatch(){
+    watcher = fs.watch(storeID, function (event, filename) {
+        switch (filename) {
+            case 'items.json':
+                grabImageFields();
+                break;
+            case 'images.json':
+                downloadImages();
+                break;
+        }
+    });
+}
+
+var buildActions = {
     newXML: function (){
         createDIR(storeID);
         createDIR(storeID + '/images');
+        startWatch();
         getFile(url, storeID + '/catalog.xml', startXmlParse);
     },
     updateXML: function (){
         this.new();
     },
     rebuildXML: function () {
-        startXmlParse();
+        if (fs.existsSync(storeID)) {
+            startXmlParse();
+            startWatch();
+        } else {
+            console.log('Running new as ' + storeID + '\'s folder does not exist');
+            this.new();
+        }
     },
     imagesXML: function () {
-        grabImageFields();
+        //grabImageFields();
+        //startWatch();
+        downloadImages();
     }
 }
 
